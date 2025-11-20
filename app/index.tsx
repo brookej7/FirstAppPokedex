1473172a6973d4a3f1d9b7a92151e1d880f5d75f
Brooke <brooksta21.bj@gmail.com>
2025-11-20 17:21:21 +1000

    Added in colour grading and styling to each element, colour is based on first type listed for each pokemon from the API

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,39 @@
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 interface Pokemon {
   name : string;
   image: string;
   imageBack: string;
+  types: PokemonType[];
+}
+
+interface PokemonType {
+  type: {
+    name : string;
+    ulr: string;
+  }
+}
+
+const coloursByType = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  grass: "#7AC74C",
+  electric: "#F7D02C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
 }
 
 export default function Index() {
@@ -31,6 +60,7 @@ export default function Index() {
             name : pokemon.name,
             image: details.sprites.front_default, //main sprite
             imageBack: details.sprites.back_default, //main sprite
+            types: details.types,
           };
         })
       )
@@ -41,11 +71,34 @@ export default function Index() {
   }
 
   return (
-    <ScrollView>
+    <ScrollView
+      contentContainerStyle={{
+        gap: 16,
+        padding: 16,
+      }}
+    >
       {pokemons.map((pokemon) => (
-        <View key={pokemon.name}>
-          <Text>{pokemon.name}</Text>
-          <Image 
+        <View 
+          key={pokemon.name} 
+          style={{
+            // @ts-ignore
+            backgroundColor: coloursByType[pokemon.types[0].type.name] + 20,
+            padding: 20,
+            borderRadius: 20
+          }}
+        >
+          <Text style={styles.name}>{pokemon.name}</Text>
+          {pokemon.types.map((pokemonType) => (
+              <View key={pokemonType.type.name}>
+                <Text style={styles.type}>{pokemonType.type.name}</Text> 
+              </View>
+              // <Text>{pokemon.types[0].type.name}</Text> old versio for one only
+          ))}
+        
+          <View style={{
+            flexDirection: "row"
+          }}>
+            <Image 
             source={{uri: pokemon.image}}
             style={{ width: 150, height: 150}}
           />
@@ -53,8 +106,23 @@ export default function Index() {
             source={{uri: pokemon.imageBack}}
             style={{ width: 150, height: 150}}
           />
+          </View>
+          
         </View>
       ))}
     </ScrollView>
   );
 }
+
+
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: 'center'
+  },
+  type: {
+    fontSize: 20,
+    color: "gray",
+  },
+});
